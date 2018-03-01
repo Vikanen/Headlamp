4618478bf9f17bc137255e6d893730248ad5870a
Vikanen <janne.keckman@gmail.com>
2018-03-01 14:13:32 +0200

    Fix lamp directions _AGAIN_
    Lamp doesn't appear in compost anymore. Also cleaned up code and changed the way mod searches for player's head

--- Headlamp/HeadLamp.cs
+++ Headlamp/HeadLamp.cs
@@ -1,6 +1,5 @@
 ﻿using MSCLoader;
 using UnityEngine;
-using System.Threading;
 
 namespace RallyLights
 {
@@ -9,48 +8,45 @@ namespace RallyLights
         public override string ID => "Headlamp";
         public override string Name => "Headlamp";
         public override string Author => "Vikanen";
-        public override string Version => "1.0";
+        public override string Version => "1.1";
 
         private Keybind LampKey = new Keybind("KeyID", "Key name", KeyCode.KeypadMinus);
 		
 		private bool LampOff;
-		public GameObject PLAYER;
+        public static bool fullyLoaded = false;
+        public GameObject PLAYER;
 		private GameObject lightGameObject;
 
 		public override void OnLoad()
         {
             Keybind.Add(this, LampKey);
-            new Thread(FindPlayer).Start();
-        }
-
-        void FindPlayer()
-        {
-            Thread.Sleep(9000);
-            PLAYER = GameObject.Find("FPSCamera");
             LampOff = true;
-            new Thread(LoadLight).Start();
         }
-
-        void LoadLight()
-        {
-            Thread.Sleep(1000);
-
-            lightGameObject = new GameObject("HeadlampLight");
-            Light lightComp = lightGameObject.AddComponent<Light>();
-            lightComp.color = Color.white;
-            lightComp.type = LightType.Spot;
-            lightComp.spotAngle = 70;
-            lightComp.intensity = 5;
-            lightComp.range = 20;
-            lightGameObject.transform.parent = PLAYER.transform;
-            lightGameObject.transform.localPosition = new Vector3(0, 0, 0);
-            lightGameObject.transform.localEulerAngles = new Vector3(0, 0, 0);
-        }
-
 
         public override void Update()
-        {	
-			if (LampKey.IsDown())
+        {
+
+            if (!fullyLoaded)
+            {
+                if (GameObject.Find("PLAYER") != null)
+                {
+                    PLAYER = GameObject.Find("FPSCamera");
+                    fullyLoaded = true;
+
+                    lightGameObject = new GameObject("HeadlampLight");
+                    Light lightComp = lightGameObject.AddComponent<Light>();
+                    lightComp.color = Color.white;
+                    lightComp.type = LightType.Spot;
+                    lightComp.spotAngle = 70;
+                    lightComp.intensity = 5;
+                    lightComp.range = 20;
+                    lightGameObject.transform.parent = PLAYER.transform;
+                    lightGameObject.transform.localPosition = new Vector3(0, 0, 0);
+                    lightGameObject.transform.localEulerAngles = new Vector3(0, 0, 0);
+                }
+            }
+
+            if (LampKey.IsDown())
             {
                 LampOff = !LampOff;
             }
